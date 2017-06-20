9e3ad6f4548900ded122e848fa7ae808bfa4722b
Andrew Smith <andrw.l.smth@gmail.com>
2017-06-20 14:45:54 -0400

    Add user class for authentication

---
+++ NetCommToolsLib/src/CommModels/User.java
@@ -0,0 +1,89 @@
+package CommModels;
+
+import java.io.Serializable;
+
+/**
+ * Data model for holding info about a user including their username, password, first and last names, admin status and
+ * associated schedule.
+ */
+public class User implements Serializable
+{
+    private String userName;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private boolean isValid; // true if username and password have been validated (found in db)
+
+    public User(String un, String p, String e, String fn, String ln, String r)
+    {
+        this.userName = un;
+        this.password = p;
+        this.email = e;
+        this.firstName = fn;
+        this.lastName = ln;
+        this.role = r;
+        this.isValid = false;
+    }
+
+    public String getUserName()
+    {
+        return this.userName;
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+    public String getRole()
+    {
+        return this.role;
+    }
+
+    public boolean getValidity()
+    {
+        return this.isValid;
+    }
+
+    public void setEmail(String e)
+    {
+        this.email = e;
+    }
+
+    public void setFirstName(String fn)
+    {
+        this.firstName = fn;
+    }
+
+    public void setLastName(String ln)
+    {
+        this.lastName = ln;
+    }
+
+    public void setRole(String r)
+    {
+        this.role = r;
+    }
+
+    public void setValidity(boolean v)
+    {
+        this.isValid = v;
+    }
+}
