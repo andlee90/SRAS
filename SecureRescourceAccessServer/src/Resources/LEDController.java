0686b1ccd99c3f632d4a5db3e43ef20e4b62c9d6
asmith <andrw.l.smth@gmail.com>
2017-07-24 09:35:22 -0400

    Clean up the led controller a little.

--- SecureRescourceAccessServer/src/Resources/LEDController.java
+++ SecureRescourceAccessServer/src/Resources/LEDController.java
@@ -1,9 +1,6 @@
 package Resources;
 
-import CommModels.Command;
-import CommModels.Device;
-import CommModels.DeviceStatus;
-import CommModels.Led;
+import CommModels.*;
 import Main.Main;
 import com.pi4j.io.gpio.*;
 
@@ -13,44 +10,70 @@ import java.util.Collections;
 
 /**
  * Implementation for controlling a single LED.
+ * Commented lines are commented to allow testing outside of a Raspbian environment. Uncomment before testing on a Pi.
  */
 public class LEDController implements DeviceController
 {
-    private Led device;                  // The Device being controlled
-    private GpioController gpio;
-    private GpioPinDigitalOutput pin;       // The pin to which the device in connected
+    private Led device;                     // The device being controlled.
+    private GpioController gpio;            // The controller for the device.
+    private GpioPinDigitalOutput pin;       // The pin to which the device in connected.
+    private boolean state;                  // True if ON, false if OFF
 
     LEDController(Device d)
     {
         this.device = (Led) d;
 
         //gpio = GpioFactory.getInstance();
-        //pin = gpio.provisionDigitalOutputPin(getGpioPin(device.getDevicePin()), "MyLED", PinState.LOW);
+        //pin = gpio.provisionDigitalOutputPin(getGpioPin(device.getDevicePin()), device.getDeviceName(), PinState.LOW);
+        //pin.setShutdownOptions(true, PinState.LOW);
     }
 
+    /**
+     * Notifies the user about whether or not the device is available for use.
+     */
     @Override
     public boolean isAvailable()
     {
         return device.getDeviceStatus().equals(DeviceStatus.AVAILABLE);
     }
 
+    /**
+     * Issues a command to the device.
+     * @param ct the type of command being issued.
+     */
     @Override
     public void issueCommand(Command.CommandType ct)
     {
-        //pin.setShutdownOptions(true, PinState.LOW);
-
         if (ct == Command.CommandType.TOGGLE)
         {
+            if (device.getDeviceState() == LedState.BLINKING)
+            {
+                // If the device is blinking, first turn it off.
+                pin = gpio.provisionDigitalOutputPin(getGpioPin(device.getDevicePin()), device.getDeviceName(), PinState.LOW);
+                state = false;
+            }
+            if (state)
+            {
+                device.setDeviceState(LedState.OFF);
+                state = false;
+            }
+            else
+            {
+                device.setDeviceState(LedState.ON);
+                state = true;
+            }
+
+            //pin.toggle();
             System.out.println("> [" + Main.getDate() + "] "
                     + device.getDeviceName() + " on "
                     + device.getDevicePin() + ": pin state toggled.");
-            //pin.toggle();
         }
         else if (ct == Command.CommandType.BLINK)
         {
             System.out.println("> [" + Main.getDate() + "] "
                     + device.getDeviceName() + " on "
                     + device.getDevicePin() + ": pin is blinking.");
+            device.setDeviceState(LedState.BLINKING);
             //pin.blink(100);
         }
     }
@@ -78,7 +101,6 @@ public class LEDController implements DeviceController
                     resultPin = pin;
                     break;
                 }
-                break;
             }
             else if (Integer.toString(x).length() == 1)
             {
